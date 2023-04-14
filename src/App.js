202443f5ba380af336a0238e82f7b132cb70534b
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-13 21:03:38 -0400

    Add Bootstrap CDN and set up favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <p className="badge bg-warning">test</p>
     </div>
   );
 }
