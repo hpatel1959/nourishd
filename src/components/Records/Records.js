585a5b9806742000fecee5ac53238267e6e70b77
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-17 18:00:40 -0400

    Refactor code

--- src/components/Records/Records.js
+++ src/components/Records/Records.js
@@ -2,20 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Records() {
-  const [userData, setUserData] = useState({})
+  const [userData, setUserData] = useState({});
 
   const fetchUserData = async () => {
     const url = "http://localhost:4000/userInfo";
 
     try {
-      const response = await axios.get(url, { withCredentials: true })
-      console.log(response.data.user)
-      setUserData(response.data.user)
+      const response = await axios.get(url, { withCredentials: true });
+      console.log(response.data.user);
+      setUserData(response.data.user);
     } catch (error) {
       console.error("Error: " + error.message);
     }
-    
-  }
+  };
 
   useEffect(() => {
     fetchUserData(); // Invoke the async function within useEffect
