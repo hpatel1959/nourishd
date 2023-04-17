432a4cdc9eb796275252de79dca46a59a114aa79
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-17 10:51:29 -0400

    Add logout function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,32 @@
+import axios from "axios";
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const logOut = async (e) => {
+    e.preventDefault();
+
+    const url = "http://localhost:4000/logout";
+
+    const response = await axios.get(url, { withCredentials: true });
+    try {
+      if (response.data.success) {
+        navigate("/");
+      } else {
+        console.log("Login failed: " + response.data.message);
+      }
+    } catch (error) {
+      console.error("Error during login: " + error.message);
+    }
+  };
+
+  const sessionCookie = document.cookie.replace(
+    /(?:(?:^|.*;\s*)_nutrition_app_api_session\s*\=\s*([^;]*).*$)|^.*$/,
+    "$1"
+  );
+  const isLoggedIn = sessionCookie !== null;
+
   return (
     <nav className="navbar navbar-expand bg-warning flex-column">
       <ul className="navbar-nav">
@@ -31,14 +56,19 @@ function Navbar() {
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/login">
-            Login
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/logout">
-            Logout
-          </NavLink>
+          {isLoggedIn ? (
+            <li className="nav-item">
+              <button className="btn btn-link nav-link" onClick={logOut}>
+                Logout
+              </button>
+            </li>
+          ) : (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </li>
+          )}
         </li>
       </ul>
     </nav>
