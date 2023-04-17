e81a236054ddae4757e41ea6a7280596cc64a49a
Harsh Patel <hpatel1959@hotmai.com>
2023-04-17 18:11:43 -0400

    add conditional render for user details

--- src/components/Records/Records.js
+++ src/components/Records/Records.js
@@ -2,20 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Records() {
-  const [userData, setUserData] = useState({})
+  const [userData, setUserData] = useState({});
 
   const fetchUserData = async () => {
     const url = "http://localhost:4000/userInfo";
 
     try {
-      const response = await axios.get(url, { withCredentials: true })
-      console.log(response.data.user)
-      setUserData(response.data.user)
+      const response = await axios.get(url, { withCredentials: true });
+      console.log(response.data.user);
+      setUserData(response.data.user);
     } catch (error) {
       console.error("Error: " + error.message);
     }
-    
-  }
+  };
 
   useEffect(() => {
     fetchUserData(); // Invoke the async function within useEffect
@@ -24,7 +23,16 @@ function Records() {
   return (
     <div>
       <h1>Records</h1>
-      <p>{userData.username}</p>
+      {userData ? (
+        <>
+          <p>{userData.username}</p>
+          <p>{userData.age} years old</p>
+          <p>{userData.height} cm</p>
+          <p>{userData.wieght} kg</p>
+        </>
+      ) : (
+        <p>Please log in</p>
+      )}
     </div>
   );
 }
