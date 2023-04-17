4f81d5abf47a2d332fbaab0ac21305a0647ededa
Harsh Patel <hpatel1959@hotmai.com>
2023-04-17 16:59:00 -0400

    add axios request for records page to fetch user data

--- src/components/Records/Records.js
+++ src/components/Records/Records.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Records() {
+  const [userData, setUserData] = useState({})
+
+  const fetchUserData = async () => {
+    const url = "http://localhost:4000/userInfo";
+
+    try {
+      const response = await axios.get(url, { withCredentials: true })
+      console.log(response.data.user)
+    } catch (error) {
+      console.error("Error: " + error.message);
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchUserData(); // Invoke the async function within useEffect
+  }, []);
+
   return (
     <div>
       <h1>Records</h1>
