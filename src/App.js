3977a693800af1b5871850d42cb221d3a0c5aa55
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-17 12:30:28 -0400

    Fix syntax error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //REACT-ROUTER-DOM
 import { Route, Routes } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
 
 import "./App.css";
 
@@ -13,23 +13,44 @@ import Login from "./components/Forms/Login";
 import Home from "./components/Home";
 
 function App() {
-
   const [logInStatus, setLogInStatus] = useState(false);
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-      <div className="App">
-        <Navbar logInStatus={logInStatus} setLogInStatus={setLogInStatus}/>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/records" element={<Records />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/favorites" element={<Favourites />} />
-          <Route path="/signup" element={<SignUp setLogInStatus={setLogInStatus} email={email} setEmail={setEmail} password={password} setPassword={setPassword}/>}/>} />
-          <Route path="/login" element={<Login setLogInStatus={setLogInStatus} email={email} setEmail={setEmail} password={password} setPassword={setPassword}/>} />
-        </Routes>
-      </div>
+    <div className="App">
+      <Navbar logInStatus={logInStatus} setLogInStatus={setLogInStatus} />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/records" element={<Records />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/favorites" element={<Favourites />} />
+        <Route
+          path="/signup"
+          element={
+            <SignUp
+              setLogInStatus={setLogInStatus}
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPassword={setPassword}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Login
+              setLogInStatus={setLogInStatus}
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPassword={setPassword}
+            />
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
