a6e002761e67dbb11097713f978e68bd8b957be6
Harsh Patel <hpatel1959@hotmai.com>
2023-04-18 12:49:20 -0400

    attempt to change displayed goal value to show in correct units

--- src/components/Records/NutrientListItem.js
+++ src/components/Records/NutrientListItem.js
@@ -7,15 +7,34 @@ function NutrientListItem(props) {
   const valObj = Object.values(data);
   const currentVal = valObj[0].current;
   const suggestedVal = valObj[0].suggested;
-  const goal = Math.floor(suggestedVal - currentVal);
+  const remaining = Math.floor(suggestedVal - currentVal);
   console.log(valObj);
   return (
     <div>
       <p>
-        {lable} : {currentVal} / {suggestedVal} | Remaining {goal}
+        {lable} : {currentVal} / {suggestedVal} | Remaining {remaining}
       </p>
     </div>
   );
 }
 
 export default NutrientListItem;
+
+// {
+//     <div>
+//       if (suggestedVal.isInteger) {
+//        <p>
+//         {lable} (g) : {currentVal} / {suggestedVal} | Remaining {remaining}
+//       </p>
+//       } elseif (suggestedVal < 0 && (suggested * 1000).isInteger) {
+//         <p>
+//         {lable} (mg) : {currentVal} / {suggestedVal} | Remaining {remaining}
+//         </p>
+//       } else (suggestedVal < 0 && (suggestedVal * 1000000).isInteger) {
+//         <p>
+//         {lable} (mcg) : {currentVal} / {suggestedVal} | Remaining {remaining}
+//         </p>
+//       }
+
+//     </div>
+// }
