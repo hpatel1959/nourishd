ce1edb9776f3e7493a40ba670b475934d1115e0e
Harsh Patel <hpatel1959@hotmai.com>
2023-04-21 13:35:23 -0400

    add error messages for invalid input on login and sign up pages

--- src/components/Forms/Login.js
+++ src/components/Forms/Login.js
@@ -1,57 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
 
 function Login(props) {
-
   const { setLogInStatus, email, setEmail, password, setPassword } = props;
   const navigate = useNavigate();
+  const [showLoginError, setShowLoginError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
-  }
+  };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-  }
+  };
 
   const handleSubmit = async (event) => {
-
     event.preventDefault();
 
-    const url = 'http://localhost:4000/login'
+    const url = "http://localhost:4000/login";
 
     const requestData = {
       email: email,
-      password: password
+      password: password,
     };
 
     try {
       const response = await axios.post(url, requestData, { withCredentials: true });
 
       if (response.data.success) {
-        setLogInStatus(true)
+        if (!showLoginError) {
+          setShowLoginError(false);
+        }
+        setLogInStatus(true);
         navigate("/");
       } else {
-        console.log("Login failed: " + response.data.message);
+        setShowLoginError(true);
+        setErrorMessage(response.data.message);
       }
     } catch (error) {
-        console.error("Error during login: " + error.message);
-    };
-
-  }
+      console.error("Error during login: " + error.message);
+    }
+  };
 
   return (
     <>
-    <div>
-      <p>Log In</p>
-      <form method="POST" onSubmit={handleSubmit}>
-      <input type="text" name="email" placeholder="Email address" onChange={handleEmailChange}></input>
-      <input type="text" name="password" placeholder="Password" onChange={handlePasswordChange}></input>
-      <button type="submit">Login</button>
-      </form>
-    </div>
+      <div>
+        <p>Log In</p>
+        <form method="POST" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email address"
+            onChange={handleEmailChange}
+          ></input>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handlePasswordChange}
+          ></input>
+          <button type="submit">Login</button>
+          {showLoginError && (
+            <div className="error-message-container">
+              <p className="alert alert-danger">{errorMessage}</p>
+            </div>
+          )}
+        </form>
+      </div>
     </>
   );
 }
