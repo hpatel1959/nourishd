d135be84612f2405d80c865c0bd04015b355f7ac
Harsh Patel <hpatel1959@hotmai.com>
2023-04-21 02:10:57 -0400

    move custom meal form to own branch

---
+++ src/components/CustomMealForm.js
@@ -0,0 +1,107 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+function CustomMealForm(props) {
+  const { setShowCustomRecipeForm, triggerUpdate } = props;
+  const [mealName, setMealName] = useState("");
+  const [calories, setCalories] = useState("");
+  const [fat, setFat] = useState("");
+  const [protein, setProtein] = useState("");
+  const [fiber, setFiber] = useState("");
+  const [sugar, setSugar] = useState("");
+  const [carbohydrates, setCarbohydrates] = useState("");
+
+  const handleAddMealClick = async (event) => {
+    event.preventDefault();
+    setShowCustomRecipeForm(false);
+
+    const url = "http://localhost:4000/addCustomMealNutrients";
+
+    const requestData = {
+      day: {
+        calories,
+        fat,
+        carbohydrates,
+        sugar,
+        protein,
+        fiber,
+        history: { [mealName]: null },
+      },
+    };
+
+    const response = await axios.post(url, requestData, { withCredentials: true });
+
+    if (response.data.success) {
+      console.log("Saved");
+    } else {
+      console.log("Failed");
+    }
+
+    triggerUpdate();
+  };
+
+  const handleMealNameChange = (event) => {
+    setMealName(event.target.value);
+  };
+
+  const handleCalorieChange = (event) => {
+    setCalories(event.target.value);
+  };
+
+  const handleCarbohydratesChange = (event) => {
+    setCarbohydrates(event.target.value);
+  };
+
+  const handleFatChange = (event) => {
+    setFat(event.target.value);
+  };
+
+  const handleSugarChange = (event) => {
+    setSugar(event.target.value);
+  };
+
+  const handleFiberChange = (event) => {
+    setFiber(event.target.value);
+  };
+
+  const handleProteinChange = (event) => {
+    setProtein(event.target.value);
+  };
+
+  return (
+    <form method="POST">
+      <input
+        type="text"
+        name="meal-name"
+        placeholder="Meal name"
+        onChange={handleMealNameChange}
+      ></input>
+      <input
+        type="number"
+        name="calories"
+        placeholder="Calories"
+        onChange={handleCalorieChange}
+      ></input>
+      <input type="number" name="fat" placeholder="Fat(g)" onChange={handleFatChange}></input>
+      <input
+        type="number"
+        name="carbohydrates"
+        placeholder="Carbohydrates(g)"
+        onChange={handleCarbohydratesChange}
+      ></input>
+      <input type="number" name="sugar" placeholder="Sugar(g)" onChange={handleSugarChange}></input>
+      <input type="number" name="fiber" placeholder="Fiber(g)" onChange={handleFiberChange}></input>
+      <input
+        type="number"
+        name="protein"
+        placeholder="Protein(g)"
+        onChange={handleProteinChange}
+      ></input>
+      <button type="submit" onClick={handleAddMealClick}>
+        Add Meal
+      </button>
+    </form>
+  );
+}
+
+export default CustomMealForm;
--- src/components/Records/Records.js
+++ src/components/Records/Records.js
@@ -4,18 +4,12 @@ import axios from "axios";
 import UserInfo from "./UserInfo";
 import NutrientList from "./NutrientList";
 import MealHistoryList from "./MealHistoryList";
+import CustomMealForm from "../CustomMealForm";
 
 function Records() {
   const [updateKey, setUpdateKey] = useState(0);
   const [userData, setUserData] = useState({});
   const [showCustomRecipeForm, setShowCustomRecipeForm] = useState(false);
-  const [mealName, setMealName] = useState("");
-  const [calories, setCalories] = useState("");
-  const [fat, setFat] = useState("");
-  const [protein, setProtein] = useState("");
-  const [fiber, setFiber] = useState("");
-  const [sugar, setSugar] = useState("");
-  const [carbohydrates, setCarbohydrates] = useState("");
 
   const navigate = useNavigate();
 
@@ -38,65 +32,8 @@ function Records() {
     setShowCustomRecipeForm(true);
   };
 
-  const handleAddMealClick = async (event) => {
-    event.preventDefault();
-    setShowCustomRecipeForm(false);
-
-    const url = "http://localhost:4000/addCustomMealNutrients";
-
-    const requestData = {
-      day: {
-        calories,
-        fat,
-        carbohydrates,
-        sugar,
-        protein,
-        fiber,
-        history: { [mealName]: null },
-      },
-    };
-
-    const response = await axios.post(url, requestData, { withCredentials: true });
-
-    if (response.data.success) {
-      console.log("Saved");
-    } else {
-      console.log("Failed");
-    }
-
-    triggerUpdate();
-  };
-
-  const handleMealNameChange = (event) => {
-    setMealName(event.target.value);
-  };
-
-  const handleCalorieChange = (event) => {
-    setCalories(event.target.value);
-  };
-
-  const handleCarbohydratesChange = (event) => {
-    setCarbohydrates(event.target.value);
-  };
-
-  const handleFatChange = (event) => {
-    setFat(event.target.value);
-  };
-
-  const handleSugarChange = (event) => {
-    setSugar(event.target.value);
-  };
-
-  const handleFiberChange = (event) => {
-    setFiber(event.target.value);
-  };
-
-  const handleProteinChange = (event) => {
-    setProtein(event.target.value);
-  };
-
   useEffect(() => {
-    fetchUserData(); // Invoke the async function within useEffect
+    fetchUserData();
   }, [updateKey]);
 
   return (
@@ -114,53 +51,10 @@ function Records() {
             Add Custom Meal
           </button>
           {showCustomRecipeForm && (
-            <form method="POST">
-              <input
-                type="text"
-                name="meal-name"
-                placeholder="Meal name"
-                onChange={handleMealNameChange}
-              ></input>
-              <input
-                type="number"
-                name="calories"
-                placeholder="Calories"
-                onChange={handleCalorieChange}
-              ></input>
-              <input
-                type="number"
-                name="fat"
-                placeholder="Fat(g)"
-                onChange={handleFatChange}
-              ></input>
-              <input
-                type="number"
-                name="carbohydrates"
-                placeholder="Carbohydrates(g)"
-                onChange={handleCarbohydratesChange}
-              ></input>
-              <input
-                type="number"
-                name="sugar"
-                placeholder="Sugar(g)"
-                onChange={handleSugarChange}
-              ></input>
-              <input
-                type="number"
-                name="fiber"
-                placeholder="Fiber(g)"
-                onChange={handleFiberChange}
-              ></input>
-              <input
-                type="number"
-                name="protein"
-                placeholder="Protein(g)"
-                onChange={handleProteinChange}
-              ></input>
-              <button type="submit" onClick={handleAddMealClick}>
-                Add Meal
-              </button>
-            </form>
+            <CustomMealForm
+              triggerUpdate={triggerUpdate}
+              setShowCustomRecipeForm={setShowCustomRecipeForm}
+            />
           )}
 
           <MealHistoryList updateKey={updateKey} triggerUpdate={triggerUpdate} />
