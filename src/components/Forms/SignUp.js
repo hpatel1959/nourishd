71d7bfd370270933a5814d8b0fe7e598be944fd0
Harsh Patel <hpatel1959@hotmai.com>
2023-04-24 14:16:25 -0400

    add styling to signup page

--- src/components/Forms/SignUp.js
+++ src/components/Forms/SignUp.js
@@ -1,6 +1,8 @@
 import { React, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./SignUp.css";
+import loginLogo from "../../img/login-logo.png";
 
 function Signup(props) {
   const { setLogInStatus, email, setEmail, password, setPassword } = props;
@@ -132,73 +134,171 @@ function Signup(props) {
   };
 
   return (
+    // <>
+    //   <div>
+    //     <p>Sign Up!</p>
+    //     <form method="POST" onSubmit={handleSubmit}>
+    //       <input
+    //         type="text"
+    //         name="username"
+    //         placeholder="Username"
+    //         onChange={handleUsernameChange}
+    //       ></input>
+    //       <input
+    //         type="text"
+    //         name="email"
+    //         placeholder="Email address"
+    //         onChange={handleEmailChange}
+    //       ></input>
+    //       <input
+    //         type="password"
+    //         name="password"
+    //         placeholder="Password"
+    //         onChange={handlePasswordChange}
+    //       ></input>
+    //       <input
+    //         type="password"
+    //         name="passwordConfirmation"
+    //         placeholder="Confirm password"
+    //         onChange={handlePasswordConfirmationChange}
+    //       ></input>
+    //       <input
+    //         type="number"
+    //         name="height"
+    //         placeholder="Height"
+    //         onChange={handleHeightChange}
+    //       ></input>
+    //       <input
+    //         type="number"
+    //         name="weight"
+    //         placeholder="Weight"
+    //         onChange={handleWeightChange}
+    //       ></input>
+    //       <input type="number" name="age" placeholder="Age" onChange={handleAgeChange}></input>
+    //       <div>
+    //         <input
+    //           type="radio"
+    //           id="male"
+    //           name="sex"
+    //           value="male"
+    //           onChange={handleSexChange}
+    //         ></input>
+    //         <label htmlFor="male">Male</label>
+    //         <input
+    //           type="radio"
+    //           id="female"
+    //           name="sex"
+    //           value="female"
+    //           onChange={handleSexChange}
+    //         ></input>
+    //         <label htmlFor="female">Female</label>
+    //       </div>
+    //       <button type="submit">Sign up</button>
+    //     </form>
+    //   </div>
+    // {showSignUpError && (
+    //   <div className="error-message-container">
+    //     <p className="alert alert-danger">{errorMessage}</p>
+    //   </div>
+    // )}
+    // </>
+
     <>
-      <div>
-        <p>Sign Up!</p>
-        <form method="POST" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={handleUsernameChange}
-          ></input>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email address"
-            onChange={handleEmailChange}
-          ></input>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={handlePasswordChange}
-          ></input>
-          <input
-            type="password"
-            name="passwordConfirmation"
-            placeholder="Confirm password"
-            onChange={handlePasswordConfirmationChange}
-          ></input>
-          <input
-            type="number"
-            name="height"
-            placeholder="Height"
-            onChange={handleHeightChange}
-          ></input>
-          <input
-            type="number"
-            name="weight"
-            placeholder="Weight"
-            onChange={handleWeightChange}
-          ></input>
-          <input type="number" name="age" placeholder="Age" onChange={handleAgeChange}></input>
-          <div>
-            <input
-              type="radio"
-              id="male"
-              name="sex"
-              value="male"
-              onChange={handleSexChange}
-            ></input>
-            <label htmlFor="male">Male</label>
-            <input
-              type="radio"
-              id="female"
-              name="sex"
-              value="female"
-              onChange={handleSexChange}
-            ></input>
-            <label htmlFor="female">Female</label>
-          </div>
-          <button type="submit">Sign up</button>
-        </form>
-      </div>
-      {showSignUpError && (
-        <div className="error-message-container">
-          <p className="alert alert-danger">{errorMessage}</p>
+      <div className="element-wrapper">
+        <div className="logo-message-container">
+          <img className="signup-logo" src={loginLogo} alt="signup-logo" />
+          <p className="signup-message">
+            Ready to Fuel Your Journey? <strong>Sign Up.</strong>
+          </p>
         </div>
-      )}
+        <div className="signup-card">
+          <form method="POST" onSubmit={handleSubmit}>
+            <div className="form-group input-container">
+              <input
+                className="form-control"
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={handleUsernameChange}
+              ></input>
+              <input
+                className="form-control"
+                type="text"
+                name="email"
+                placeholder="Email address"
+                onChange={handleEmailChange}
+              ></input>
+              <input
+                className="form-control"
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handlePasswordChange}
+              ></input>
+              <input
+                className="form-control"
+                type="password"
+                name="passwordConfirmation"
+                placeholder="Confirm password"
+                onChange={handlePasswordConfirmationChange}
+              ></input>
+              <input
+                className="form-control"
+                type="number"
+                name="height"
+                placeholder="Height"
+                onChange={handleHeightChange}
+              ></input>
+              <input
+                className="form-control"
+                type="number"
+                name="weight"
+                placeholder="Weight"
+                onChange={handleWeightChange}
+              ></input>
+              <input
+                className="form-control"
+                type="number"
+                name="age"
+                placeholder="Age"
+                onChange={handleAgeChange}
+              ></input>
+              <div className="radio-buttons-container">
+                <div className="radio-button-and-label">
+                  <input
+                    className="radio-button"
+                    type="radio"
+                    id="male"
+                    name="sex"
+                    value="male"
+                    onChange={handleSexChange}
+                  ></input>
+                  <label htmlFor="male">Male</label>
+                </div>
+                <div className="radio-button-and-label">
+                  <input
+                    className="radio-button"
+                    type="radio"
+                    id="female"
+                    name="sex"
+                    value="female"
+                    onChange={handleSexChange}
+                  ></input>
+                  <label htmlFor="female">Female</label>
+                </div>
+              </div>
+              <button class="btn btn-outline-primary create-account-button" type="submit">
+                Sign up
+              </button>
+              {showSignUpError && (
+                <div className="error-message-container">
+                  <p className="alert alert-danger">{errorMessage}</p>
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
