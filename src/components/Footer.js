a1907cb3ff073d2cd735a529d287d96ec9fa055d
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-24 10:29:06 -0400

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,43 +2,50 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="d-flex bg-warning">
-      <div className="link-list p-2 container">
-        <ul>
-          <li>About</li>
-          <li>Developers</li>
-          <li>News</li>
-        </ul>
-      </div>
-      <div className="site-map p-2 container">
-        {" "}
-        <ul>
-          <li>Home</li>
-          <li>Recipes</li>
-          <li>Favourites</li>
-          <li>Records</li>
-          <li>Sign Up</li>
-          <li>Login</li>
-        </ul>
-      </div>
-      <div className="contact p-2 container">
-        <ul>
-          <li>Home</li>
-          <li>Recipes</li>
-          <li>Favourites</li>
-          <li>Records</li>
-          <li>Sign Up</li>
-          <li>Login</li>
-        </ul>
+    <div className="footer-container">
+      <div className="lists-wrapper">
+        <div className="link-list p-2 container">
+          <ul>
+            <li>About</li>
+            <li>Developers</li>
+            <li>News</li>
+          </ul>
+        </div>
+        <div className="site-map p-2 container">
+          {" "}
+          <ul>
+            <li>Home</li>
+            <li>Recipes</li>
+            <li>Favourites</li>
+            <li>Records</li>
+            <li>Sign Up</li>
+            <li>Login</li>
+          </ul>
+        </div>
+        <div className="contact p-2 container">
+          <ul>
+            <li>Contact Us</li>
+            <li>Terms</li>
+          </ul>
+        </div>
       </div>
       <div className="sns-icons p-2 container">
         <ul>
-          <li>Twitter</li>
-          <li>LinkedIn</li>
-          <li>FaceBook</li>
-          <li>Instgram</li>
+          <li>
+            <i class="fa-brands fa-square-twitter"></i>
+          </li>
+          <li>
+            <i class="fa-brands fa-linkedin"></i>
+          </li>
+          <li>
+            <i class="fa-brands fa-square-facebook"></i>
+          </li>
+          <li>
+            <i class="fa-brands fa-square-instagram"></i>
+          </li>
         </ul>
       </div>
+      <div className="copyright">&copy; 2023 NourishD</div>
     </div>
   );
 }
