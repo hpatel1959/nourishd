22f9b36d8b95705ee4a46915fe48772d98c38702
Koji Eguchi <koji.eguchi@hotmail.com>
2023-04-24 14:46:19 -0400

    Change home page display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from "react";
-import headerImg from "../img/header.jpg";
+import headerImg from "../img/home-img.jpg";
 
 function Header() {
   return (
-    <div>
-      <img src={headerImg} alt="header" className="header-img" />
+    <div className="img-container">
+      <div className="home-contents-wrapper">
+        <div className="home-text-wrapper">
+          {" "}
+          <h3>
+            Experience a fresh way to <br />
+            <span>manage nutrition</span>
+          </h3>
+        </div>
+
+        <div className="home-btn-wrapper">
+          <a href="" className="btn home-btn">
+            Sign Up
+          </a>
+        </div>
+
+        <div className="download-icon"></div>
+      </div>
     </div>
   );
 }
