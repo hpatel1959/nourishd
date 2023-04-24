6b2e6df104b563dde093549878c97c3c7d45e72e
Harsh Patel <hpatel1959@hotmai.com>
2023-04-24 12:46:53 -0400

    change redirect path to records upon login

--- src/components/Forms/Login.js
+++ src/components/Forms/Login.js
@@ -54,7 +54,7 @@ function Login(props) {
           setShowLoginError(false);
         }
         setLogInStatus(true);
-        navigate("/");
+        navigate("/records");
       } else {
         setShowLoginError(true);
         setErrorMessage(response.data.message);
